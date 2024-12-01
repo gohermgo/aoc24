8f657ff5a230e5ede4532d1cc357ff676b8b23f9
Herman Gohn <gohnherman@gmail.com>
2024-12-01 18:48:18 +0100

    added templates for each day

---
+++ template/lib.rs
@@ -0,0 +1,17 @@
+pub mod part01 {
+    //! Part 1 solution
+}
+pub mod part02 {
+    //! Part 2 solution
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod part01 {
+        use super::*;
+    }
+    mod part02 {
+        use super::*;
+    }
+}
